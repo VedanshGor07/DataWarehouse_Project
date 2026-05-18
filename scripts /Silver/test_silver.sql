dcc87a308d9bb76bcf05389f4a75aba90405b5b1
VedanshGor07 <vedansh07gor@gmail.com>
2026-05-18 00:19:17 -0400

    Enhance data quality checks and standardization in SQL
    Refactor SQL script for data quality checks in crm_cust_info and crm_prdct_info. Added trimming for names, improved null and negative checks, and standardized data insertion.

--- scripts /Silver/test_silver.sql
+++ scripts /Silver/test_silver.sql
@@ -1,3 +1,74 @@
+-- ######### Performing Quality For crm_cust_info ######### --
+
+-- 1) Checking data duplicasy  -- 
+
+select cst_id , count(*) from bronze.crm_cust_info -- Identifing duplicates in the table
+group by cst_id
+having count(*) >=2 or cst_id is null ;
+
+select * from bronze.crm_cust_info -- checking an example of one customer for duplicate cst_id
+where cst_id =29466 ;
+
+-- Reverify The null in Primary key
+select * from bronze.crm_cust_info 
+where cst_id is null ;
+
+-- Removed Duplicates and kept the latest entries of the tables if there are any duplicates.
+
+select 
+		* 
+From (
+		select * ,
+		row_number() over  (partition by cst_id order by cst_create_date desc) as latest_flag_data
+		From bronze.crm_cust_info 
+	)t 
+where latest_flag_data =1  ; 
+
+
+-- 2) Checking for unwanted spaces
+select * from bronze.crm_cust_info    -- This part of code will check irregular names because of  spaces for First name
+where cst_firstname != trim(cst_firstname) ;
+
+select * from bronze.crm_cust_info    -- This part of code will check irregular names because of  spaces For Last name
+where cst_lastname != trim(cst_lastname) ;
+
+
+-- NOW  making it better with trimming
+-- MAINTAINING STANDARDIZATION AND CONSISTANCY
+INSERT INTO silver.crm_cust_info (  -- INSERTING TRANSFORMED DATA TO SILVER LAYER ( In crm_cust_info )
+cst_id ,
+cst_key ,
+cst_firstname ,
+cst_lastname ,
+cst_material_status,
+cst_gndr ,
+cst_create_date ) 
+
+select 
+	cst_id ,
+    cst_key ,
+    TRIM(cst_firstname) as cst_firstname,
+    TRIM(cst_lastname) as cst_lastname ,
+    CASE WHEN cst_material_status = 'M'THEN 'Married'
+        WHEN cst_material_status = 'S' THEN 'Single' 
+		ELSE 'N/A' 
+	END cst_material_status,
+    CASE WHEN  cst_gndr = 'M' THEN 'Male'
+        WHEN cst_gndr = 'F'  THEN 'Female' 
+		ELSE 'N/A'
+	END cst_gndr,
+    cst_create_date
+From bronze.crm_cust_info   ;
+
+-- ######### Performin Data Quality checks in crm_prd_info ######### --
+
+
+-- 1) Checking for Duplicates
+select prd_nm , count(*) from bronze.crm_prdct_info -- Identifing duplicates in the table
+group by prd_nm
+having count(*) >=2 or prd_nm is null ; -- NO DUPLICATES -- 
+
+
 
 -- CHeck for Null and Negative numbers 
 
@@ -12,37 +83,3 @@ select distinct (prd_line) from bronze.crm_prdct_info ;
 
 select * from bronze.crm_prdct_info
 where prd_start_dt > prd_end_dt
- ;
-
-SELECT NULLIF(sls_order_dt, 0) sls_order_dt
-FROM bronze.crm_sales_details 
-WHERE sls_order_dt = 0 or Length(sls_order_dt) != 8 ;
-
-SELECT NULLIF(sls_ship_dt, 0) sls_ship_dt
-FROM bronze.crm_sales_details 
-WHERE sls_ship_dt = 0 or Length(sls_ship_dt) != 8 ;
-
-
-SELECT NULLIF(sls_due_dt, 0) sls_due_dt
-FROM bronze.crm_sales_details 
-WHERE sls_due_dt = 0 or Length(sls_due_dt) != 8 ;
-
--- Check For Invalid date orders
-select * From bronze.crm_sales_details
-where sls_order_dt > sls_ship_dt OR sls_order_dt >sls_due_dt;
-
-select * from bronze.crm_sales_Details
-where  sls_sales != sls_quantity * sls_price 
-		or
-        sls_sales is null or sls_quantity is null or sls_price is null
-        or
-        sls_sales <=0 or sls_quantity <=0 or sls_price <=0 
-Order By sls_sales,sls_quantity , sls_price
-
--- Defining rules : 
--- if sales is null negative zero then derive it using quantity and price
--- if price is zero , derive it using sales and quantity 
--- if price is negative , convert it to positive 
-
-
-		
